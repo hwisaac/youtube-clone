3b5c396426c76ef6f906d52aa37163bec9922235
hwisaac <wh2ssac@naver.com>
2022-08-27 00:19:00 +0900

    4.2 cleaning the code

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
+
+const PORT = 4000;
+
+const app = express();
+const logger = morgan("dev");
+app.use(logger);
