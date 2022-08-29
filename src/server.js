5fd8ca33b630bb875ec72aef22ccbcaeb4c4f1c7
hwisaac <wh2ssac@naver.com>
2022-08-29 18:43:25 +0900

    variables to templates

--- src/server.js
+++ src/server.js
@@ -6,6 +6,18 @@ import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
+console.log(process.cwd());
+
 const app = express();
 const logger = morgan("dev");
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views"); // src/views에서 home.pug파일 찾도록 알려주기
 app.use(logger);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+const handleListening = () =>  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
